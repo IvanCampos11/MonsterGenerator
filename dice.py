62ae539ce94434c8560f423262c8db593a3bd208
IvanCampos11 <39417756+IvanCampos11@users.noreply.github.com>
2020-08-08 16:00:21 -0400

    e

---
+++ dice.py
@@ -0,0 +1,9 @@
+from random import randint
+
+
+def dice(rolls: int, sides: int) -> int:
+    return sum(randint(1, sides) for _ in range(rolls))
+
+
+if __name__ == '__main__':
+    print(dice(1, 10))
--- moster.py
+++ moster.py
@@ -1,6 +1,25 @@
 """Documentation."""
+from random import choice
+from dice import dice
 
 
-class Mosnter:
+class Monster:
     """Baseline monster class."""
-    pass
+    element = ['Fire', 'Water', 'Air', 'Earth']
+    types = ['Dragon', 'Giant', 'Spirit']
+
+    def __init__(self, level):
+        self.name = choice(self.element)
+        self.level = level
+        self.health = {
+            'total': dice(self.level, 8),
+            'current': 0
+        }
+
+    def __str__(self):
+        return self.name
+
+
+if __name__ == '__main__':
+    m = Monster(level=1)
+    print(m)
